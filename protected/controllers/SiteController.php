8ca4331d61c69337322f130ff327a9c42ff00636
shuangping chen <worthful.laji@gmail.com>
2012-10-13 17:15:54 +0800

    little b

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -47,7 +47,7 @@ class SiteController extends Controller
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
 		if(UUserIdentity::canHaveCourses()){
-			$this->redirect(array('classroom/index/mine/1/term/1'));
+			$this->redirect(array('classRoom/index/mine/1/term/1'));
 		}
 		$this->render('index');
 	}
